b8d04ada722811a9298d838c0c55ea922f218e65
Mario <netrix4@gmail.com>
2025-03-13 18:25:25 -0700

    Refactor data source: FireBase

--- segunda-unidad/lib/getJSONData.ts
+++ segunda-unidad/lib/getJSONData.ts
@@ -1,10 +1,22 @@
 /**
  * @module JSON/Fetcher
  * @author Mario Arias netrix4@gmail>
-*/
+ */
 
 import { SmaeResponse, Categoría } from "../interfaces/SMAEResponse";
+import { initializeApp } from "firebase/app";
+import { getDatabase, get, ref } from "firebase/database";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBMHPvj9QCr46HuJxLnmxM3R752pzpAxGM",
+  authDomain: "samedatabase.firebaseapp.com",
+  databaseURL: "https://samedatabase-default-rtdb.firebaseio.com",
+  projectId: "samedatabase",
+  storageBucket: "samedatabase.firebasestorage.app",
+  messagingSenderId: "1003026858143",
+  appId: "1:1003026858143:web:2635b3b724646c6a8ae2aa",
+  measurementId: "G-9M32C6NPPG",
+};
 
 /**
  * Gets all the data from SMAE json file locallly using asyncronous fetch and looks for all the results searched by the provided keyword.
@@ -13,18 +25,21 @@ import { SmaeResponse, Categoría } from "../interfaces/SMAEResponse";
  * @returns {object[]} A list of objects thst the matched results
  */
 export const getJsonDataByKeyWord = async (keyWord: string) => {
-  try {
-    const response = await fetch(`../data/smae.json`)
-      .then((resp) => resp.json())
-      .then(
-        (alimentos: SmaeResponse[]) =>
-          alimentos.filter((item) => item.Alimento.includes(keyWord))
-        // .then((alimentos: SmaeResponse[]) => alimentos.filter((item) => item.toString() .includes(keyWord))
-      );
-    return response;
-  } catch (error) {
-    throw "Error: json failed";
-  }
+  console.log("Obtener alimentos por palabra clave");
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase(app);
+  const dbRef = ref(db, "/");
+
+  return await get(dbRef)
+    .then((snapshot) =>
+      snapshot.exists() ? snapshot.val() : console.log("Error, FireBase error")
+    )
+    .then((alimentos: SmaeResponse[]) =>
+      Object.values(alimentos).filter((item) => item.Alimento.includes(keyWord))
+    )
+    .catch((e) => {
+      console.log(e);
+    });
 };
 
 /**
@@ -33,16 +48,18 @@ export const getJsonDataByKeyWord = async (keyWord: string) => {
  * @returns {Object[]} A list of resulting objects {Alimento: 'NameOfFood'}
  */
 export const getJsonDataByCategorie = async (categorie: string) => {
-  try {
-    const response = await fetch(`../data/smae.json`)
-      .then((resp) => resp.json())
-      .then((alimentos: SmaeResponse[]) =>
-        alimentos.filter((item) => item.Categoría == categorie)
-      );
-    return response;
-  } catch (error) {
-    throw "Error: json failed";
-  }
+  console.log("Obtener alimentos por categoria");
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase(app);
+  const dbRef = ref(db, "/");
+
+  return await get(dbRef)
+    .then((snapshot) =>
+      snapshot.exists() ? snapshot.val() : console.log("Error, FireBase error")
+    )
+    .then((alimentos: SmaeResponse[]) =>
+      Object.values(alimentos).filter((item) => item.Categoría == categorie)
+    );
 };
 
 /**
@@ -50,29 +67,49 @@ export const getJsonDataByCategorie = async (categorie: string) => {
  * @returns {object[]} A list of all objects within the JSON file
  */
 export const getJsonData = async () => {
-  try {
-    const response = await fetch(`../data/smae.json`)
-      .then((resp) => resp.json())
-      .then((alimentos: SmaeResponse[]) =>
-        alimentos.map((item) => {
-          return { Alimento: item.Alimento };
-        })
-      );
-    return response;
-  } catch (error) {
-    throw "Error: json failed";
-  }
+  console.log("Obtener alimentos y regresarlos en JSONs");
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase(app);
+  const dbRef = ref(db, "/");
+
+  return await get(dbRef)
+    .then((snapshot) =>
+      snapshot.exists()
+        ? snapshot.val()
+        : console.log("No data available from FireBase")
+    )
+    .then((alimentos: SmaeResponse[]) =>
+      // Herer comes as an objects of objects and can not be send from the previous promise as such because of typede parameters
+      // still type ande legitable, tho
+      Object.values(alimentos).map((item: SmaeResponse | any) => ({
+        Alimento: item.Alimento,
+      }))
+    );
+
+  // try {
+  //   const response = await fetch(`../data/smae.json`)
+  //     // const response = await fetch(`https://samedatabase-default-rtdb.firebaseio.com/smae.json`)
+  //     .then((resp) => resp.json())
+  //     .then((alimentos: SmaeResponse[]) =>
+  //       alimentos.map((item) => {
+  //         return { Alimento: item.Alimento };
+  //       })
+  //     );
+  //   return response;
+  // } catch (error) {
+  //   throw "Error: json failed";
+  // }
 };
 
-// getJsonData()
-//   .then((alimentos) => {
-//     console.log(alimentos);
-//   })
-//   .catch((err) => console.log(err));
+getJsonData()
+  .then((alimentos) => {
+    console.log(alimentos);
+  })
+  .catch((err) => console.log(err));
 
 // const listadoCategorias = Object.values(Categoría);
 
-// const categoriaSeleccionada = listadoCategorias[15];
+// const categoriaSeleccionada = listadoCategorias[18];
 // console.log(categoriaSeleccionada, listadoCategorias.length);
 
 // getJsonDataByCategorie(categoriaSeleccionada)
@@ -81,11 +118,11 @@ export const getJsonData = async () => {
 //   })
 //   .catch((err) => console.log(err));
 
-const palabraClave = "Pan";
-console.log(`Palabra clave buscada: ${palabraClave}`)
+// const palabraClave = "cruda";
+// console.log(`Palabra clave buscada: ${palabraClave}`);
 
-getJsonDataByKeyWord(palabraClave)
-  .then((nombreAlimentos) => {
-    console.log(nombreAlimentos);
-  })
-  .catch((err) => console.log(err));
+// getJsonDataByKeyWord(palabraClave)
+//   .then((nombreAlimentos) => {
+//     console.log(nombreAlimentos);
+//   })
+//   .catch((err) => console.log(err));
